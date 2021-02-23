2a8134b93c71bb4fa1a5390426a96c0cff8323fd
Elias Calagiu <eliasc81@gmail.com>
2021-02-23 11:27:54 -0800

    created database

--- models/friends.js
+++ models/friends.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-    var Friends = sequelize.define("Post", {
+    var Friends = sequelize.define("Friends", {
         username: DataTypes.STRING,
         password: DataTypes.STRING,
         email: DataTypes.STRING,
